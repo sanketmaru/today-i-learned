e55c9f0897153a0d7fc230ab4ea81143d7639823
Sanket Maru <sanketmaru@gmail.com>
2019-05-26 10:05:02 +0530

    remove lib and universal

--- javascript/ng-lib-sank/projects/ng-lib-sank/src/lib/sank-card/sank-card.model.ts
+++
@@ -1,5 +0,0 @@
-export interface ImageData {
-    title: string;
-    description: string;
-    image: string;
-}
--- javascript/ng-lib-sank/projects/ng-lib-sank/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-lib-sank
- */
-
-export * from './lib/sank-carousel/sank-carousel.module';
-export * from './lib/sank-card/sank-card.module';
-
